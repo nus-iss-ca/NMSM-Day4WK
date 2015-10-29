fc82d571ca8a433d08cde32505d28c69e3799c1d
Pham Hoang Tuan <tuan.ph@TuanPHs-MBP.local>
2015-10-30 00:33:39 +0800

    Update main

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import question2 as question2
 import question3 as question3
 
 if __name__ == '__main__':
-    # question1.run()
-    # print("Queston 1 --- DONE!")
+    question1.run()
+    print("Queston 1 --- DONE!")
     path = "XML_Outputs"
-    # question2.run(path, "charlotte_top10.csv", "month")
-    # print("Queston 2 --- DONE!")
+    question2.run(path, "charlotte_top10.csv", "month")
+    print("Queston 2 --- DONE!")
     question3.run(path)
     print("Queston 3 --- DONE!")
